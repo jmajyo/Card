9edc5998885f73e334be90c2a217d39bc7be5315
J Matias <jmajyo@gmail.com>
2017-02-09 17:00:21 +0100

    call to decklistener

--- app/src/main/java/com/jmajyo/cards/MainActivity.java
+++ app/src/main/java/com/jmajyo/cards/MainActivity.java
@@ -3,11 +3,24 @@ package com.jmajyo.cards;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.jmajyo.cards.managers.DeckApiManager;
+import com.jmajyo.cards.model.Deck;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DeckApiManager apiManager = new DeckApiManager();
+        apiManager.setOnNewDeckListener(new DeckApiManager.DeckApimanagerNewDeckListener() {
+            @Override
+            public void onNewDeck(Deck deck) {
+
+            }
+        });
+        apiManager.newDeck(this);
     }
 }
+//https://deckofcardsapi.com/
