4503aaaa19f2cf7eb56736a823f9b728f1695e13
jmajyo-por <jmajyo@gmail.com>
2017-02-09 21:30:26 +0100

    add remains to Card model.

--- app/src/main/java/com/jmajyo/cards/model/Card.java
+++ app/src/main/java/com/jmajyo/cards/model/Card.java
@@ -10,6 +10,7 @@ public class Card {
     }
     private Suit suit;
     private String image;
+    private int remains;
 
     public Suit getSuit() {
         return suit;
@@ -25,5 +26,13 @@ public class Card {
 
     public void setImage(String image) {
         this.image = image;
+    }
+
+    public int getRemains() {
+        return remains;
+    }
+
+    public void setRemains(int remains) {
+        this.remains = remains;
     }
 }
